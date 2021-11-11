8284969a76697f82348e0b0c1a17c5b691ed81d9
AnniRo <annirdpl@yahoo.gr>
2021-11-11 18:19:18 +0200

    Add first theory quiz

--- preparatory.py
+++ preparatory.py
@@ -1,8 +1,8 @@
 # Preparatory quizzes dictionary
 
 prep_dict = {'A thirty-second note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
-             'How many 8th notes does a dotted half note have;': [3, 6, 5, 4],
-             'The Sol-La interval is equal to a;': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+             'How many 8th notes does a dotted half note have?': [3, 6, 5, 4],
+             'The Sol-La interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'The Do#-Re interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'The Fa-Sol# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'The Re-Mi# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
@@ -10,13 +10,13 @@ prep_dict = {'A thirty-second note triplet is equal in duration to a:': ['Quarte
              'The order of flats is:': ['Fa La Do Mi', 'Mi Sol Si Re Fa', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
              'How many semitones are from La to Do#?': [3, 5, 2, 4],
              'A sixteenth note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
-             'The Re-Re# interval is equal to a;': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+             'The Re-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'The Do-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'The Mi#-Fa interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'How many 8th notes does a dotted quarter note have;': [3, 7, 5, 2],
-             'How many 16th notes does a dotted quarter note have;': [6, 3, 5, 4],
-             'How many 16th notes does a quarter note have;': [3, 4, 5, 8],
-             'How many 16th notes does a half note have;': [4, 6, 8, 16],
+             'How many 8th notes does a dotted quarter note have?': [3, 7, 5, 2],
+             'How many 16th notes does a dotted quarter note have?': [6, 3, 5, 4],
+             'How many 16th notes does a quarter note have?': [3, 4, 5, 8],
+             'How many 16th notes does a half note have?': [4, 6, 8, 16],
              'The Si-Do# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'The Si-Do interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'The Mi-Fa♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
@@ -24,7 +24,7 @@ prep_dict = {'A thirty-second note triplet is equal in duration to a:': ['Quarte
              'An eighth note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
              'The La#-Si♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
              'A quarter note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
-             'How many 64th notes does a whole note have;': [8, 64, 32, 16],
+             'How many 64th notes does a whole note have?': [8, 64, 32, 16],
              'The order of sharps is:': ['Fa La Do Mi', 'Mi Sol Si Re Fa', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
              'How many semitones are from Do to Fa?': [5, 6, 4, 3],
              'How many semitones are from Re# to Fa#?': [2, 4, 3, 5],
@@ -34,14 +34,39 @@ prep_dict = {'A thirty-second note triplet is equal in duration to a:': ['Quarte
              'How many semitones are from Fa to Do#?': [6, 5, 8, 9],
              }
 
-first_dict = {'How many 16th notes does a dotted quarter note have;': [6, 3, 5, 4],
+first_dict = {'How many 16th notes does a dotted quarter note have?': [6, 3, 5, 4],
               'The Do-Re interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Sol-La interval is equal to a;': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Sol-La interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
               'The Sol-La# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Re-Re# interval is equal to a;': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Re-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
               'The Si-Do interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
               'The Mi#-Fa interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The key signature of the Do# major scale is:':['Nothing', 'Fa Do Sol', 'Fa Do', 'Fa Do Sol Re La Mi Si']
+              'What is the key signature of the Do# major scale?': ['None', 'Fa Do Sol', 'Fa Do', 'Fa Do Sol Re La Mi Si'],
+              'The order of sharps is:': ['Mi Sol Si Re Fa', 'Fa La Do Mi', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
+              'What is the key signature of the Fa# major scale?': ['Fa Do Sol Re La Mi', 'Si', 'Fa Do', 'Si Mi'],
+              'The Do#-Re interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Re-Mi# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The La#-Si♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'What is the key signature of the Do major scale?': ['Fa Do Sol Re La Mi Si', 'Si Mi La', 'Si', 'None'],
+              'What is the key signature of the Re major scale?': ['Si', 'Fa Do Sol Re', 'Fa Do', 'Fa Do Sol Re La'],
+              'The order of flats is:': ['Mi Sol Si Re Fa', 'Fa La Do Mi', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
+              'What is the key signature of the Fa major scale?': ['Fa Do Sol Re La Mi', 'Si', 'Si Mi', 'Fa Do'],
+              'What is the key signature of the La major scale?': ['Si Mi La Re', 'Si Mi La Re Sol', 'Fa Do Sol', 'Fa Do Sol Re La Mi'],
+              'How many 16th notes does a half note have?': [4, 6, 8, 16],
+              'The Mi-Fa interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Fa-Sol# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'What is the key signature of the Mi♭ major scale?': ['Si Mi La', 'Fa Do', 'Fa Do Sol Re', 'Si Mi'],
+              'What is the key signature of the Si♭ major scale?': ['Si Mi La', 'Fa Do Sol Re La', 'Si Mi', 'Fa Do Sol Re La Mi'],
+              'What is the key signature of the La♭ major scale?': ['Fa Do Sol', 'Fa Do Sol Re La Mi', 'Si Mi La Re Sol', 'Si Mi La Re'],
+              'The Do-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'How many 16th notes does a quarter note have?': [3, 4, 5, 8],
+              'The Mi-Fa♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'What is the key signature of the Mi major scale?': ['Si Mi La', 'Fa Do Sol Re', 'Fa Do', 'Fa Do Sol Re La Mi Si'],
+              'The Si#-Do interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'How many 8th notes does a dotted quarter note have?': [3, 7, 5, 2],
+              'How many 8th notes does a dotted half note have?': [3, 6, 5, 4],
+              'The Si-Do# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'What is the key signature of the Sol major scale?': ['Si Mi La Re Sol Do', 'Fa Do Sol Re', 'Si Mi La Re Sol Do Fa', 'Fa'],
               }             
 
 import pandas as pd
@@ -49,28 +74,59 @@ import random
 import time
 
 prep = pd.read_csv('preparatory.csv')
+first = pd.read_csv('first_theory.csv')
+
+pick = input('Pick a quiz:\n\nPress 1 for a preparatory quiz\nPress 2 for a 1st theory quiz\n\nYour Pick: ')
+print('-'*43)  
 
 result = 0
 
-for i in range(3):
+if pick == '1':
 
-    question, answer = random.choice(list(prep_dict.items()))
+    for i in range(3):
 
-    the_answer = prep.Answer[prep.Question == question].iloc[0]
+        question, answer = random.choice(list(prep_dict.items()))
 
-    print(f"{i+1}. {question}\n   a. {answer[0]}\n   b. {answer[1]}\n   c. {answer[2]}\n   d. {answer[3]}\n")
+        the_answer = prep.Answer[prep.Question == question].iloc[0]
 
-    guess = input('Your answer is: ')
+        print(f"{i+1}. {question}\n   a. {answer[0]}\n   b. {answer[1]}\n   c. {answer[2]}\n   d. {answer[3]}\n")
 
-    time.sleep(2)
+        guess = input('Your answer is: ')
 
-    if guess == the_answer:
-        print('\nWell done!\n')
-        result += 1
-    else:
-        print('\nIncorrect answer!\n')    
+        time.sleep(2)
 
-    time.sleep(4) 
+        if guess == the_answer:
+            print('\nWell done!\n')
+            result += 1
+        else:
+            print('\nIncorrect answer!\n')    
 
-print('-'*40)    
-print(f'You answered correctly {result} out of {i+1} questions.')
+        time.sleep(4) 
+
+    print('-'*43)    
+    print(f'You answered correctly {result} out of {i+1} questions.')    
+    
+elif pick == '2':
+
+    for i in range(3):
+
+        question, answer = random.choice(list(first_dict.items()))
+
+        the_answer = first.Answer[first.Question == question].iloc[0]    
+
+        print(f"{i+1}. {question}\n   a. {answer[0]}\n   b. {answer[1]}\n   c. {answer[2]}\n   d. {answer[3]}\n")
+
+        guess = input('Your answer is: ')
+
+        time.sleep(2)
+
+        if guess == the_answer:
+            print('\nWell done!\n')
+            result += 1
+        else:
+            print('\nIncorrect answer!\n')    
+
+        time.sleep(4) 
+
+    print('-'*43)    
+    print(f'You answered correctly {result} out of {i+1} questions.')
