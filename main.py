4b0a6024c53c936b3ffbba38dc2383b1aac83db5
AnniRo <annirdpl@yahoo.gr>
2021-11-14 22:00:14 +0200

    Update all files

--- main.py
+++ main.py
@@ -1,74 +1,74 @@
-# question and possible answers dictionary for preparatory grade
+# multiple choice quiz dictionary for preparatory grade
 prep_dict = {'A thirty-second note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
              'How many 8th notes does a dotted half note have?': [3, 6, 5, 4],
-             'The Sol-La interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Do#-Re interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Fa-Sol# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Re-Mi# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Si#-Do interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+             'The Sol-La interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Do#-Re interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Fa-Sol# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Re-Mi# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Si#-Do interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
              'The order of flats is:': ['Fa La Do Mi', 'Mi Sol Si Re Fa', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
              'How many semitones are from La to Do#?': [3, 5, 2, 4],
              'A sixteenth note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
-             'The Re-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Do-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Mi#-Fa interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+             'The Re-Re# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Do-Re# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Mi#-Fa interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
              'How many 8th notes does a dotted quarter note have?': [3, 7, 5, 2],
              'How many 16th notes does a dotted quarter note have?': [6, 3, 5, 4],
              'How many 16th notes does a quarter note have?': [3, 4, 5, 8],
              'How many 16th notes does a half note have?': [4, 6, 8, 16],
-             'The Si-Do# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Si-Do interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Mi-Fa♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+             'The Si-Do# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Si-Do interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Mi-Fa♭ interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
              'How many semitones are from Fa to Si?': [5, 6, 4, 3],
              'An eighth note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
-             'The La#-Si♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+             'The La#-Si♭ interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
              'A quarter note triplet is equal in duration to a:': ['Quarter note', 'Half note', 'Eighth note', 'Sixteenth note'],
              'How many 64th notes does a whole note have?': [8, 64, 32, 16],
              'The order of sharps is:': ['Fa La Do Mi', 'Mi Sol Si Re Fa', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
              'How many semitones are from Do to Fa?': [5, 6, 4, 3],
              'How many semitones are from Re# to Fa#?': [2, 4, 3, 5],
-             'The Sol-La# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-             'The Mi-Fa interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+             'The Sol-La# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+             'The Mi-Fa interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
              'How many semitones are from Do to the next Do?': [13, 14, 11, 12],
              'How many semitones are from Fa to Do#?': [6, 5, 8, 9],
              }
-# question and possible answers dictionary for 1st grade
+# multiple choice quiz dictionary for 1st grade
 first_dict = {'How many 16th notes does a dotted quarter note have?': [6, 3, 5, 4],
-              'The Do-Re interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Sol-La interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Sol-La# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Re-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Si-Do interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Mi#-Fa interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Do-Re interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The Sol-La interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The Sol-La# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The Re-Re# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The Si-Do interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The Mi#-Fa interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
               'What is the key signature of the Do# major scale?': ['None', 'Fa Do Sol', 'Fa Do', 'Fa Do Sol Re La Mi Si'],
               'The order of sharps is:': ['Mi Sol Si Re Fa', 'Fa La Do Mi', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
               'What is the key signature of the Fa# major scale?': ['Fa Do Sol Re La Mi', 'Si', 'Fa Do', 'Si Mi'],
-              'The Do#-Re interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Re-Mi# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The La#-Si♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Do#-Re interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The Re-Mi# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The La#-Si♭ interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
               'What is the key signature of the Do major scale?': ['Fa Do Sol Re La Mi Si', 'Si Mi La', 'Si', 'None'],
               'What is the key signature of the Re major scale?': ['Si', 'Fa Do Sol Re', 'Fa Do', 'Fa Do Sol Re La'],
               'The order of flats is:': ['Mi Sol Si Re Fa', 'Fa La Do Mi', 'Fa Do Sol Re La Mi Si', 'Si Mi La Re Sol Do Fa'],
               'What is the key signature of the Fa major scale?': ['Fa Do Sol Re La Mi', 'Si', 'Si Mi', 'Fa Do'],
               'What is the key signature of the La major scale?': ['Si Mi La Re', 'Si Mi La Re Sol', 'Fa Do Sol', 'Fa Do Sol Re La Mi'],
               'How many 16th notes does a half note have?': [4, 6, 8, 16],
-              'The Mi-Fa interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
-              'The Fa-Sol# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Mi-Fa interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
+              'The Fa-Sol# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
               'What is the key signature of the Mi♭ major scale?': ['Si Mi La', 'Fa Do', 'Fa Do Sol Re', 'Si Mi'],
               'What is the key signature of the Si♭ major scale?': ['Si Mi La', 'Fa Do Sol Re La', 'Si Mi', 'Fa Do Sol Re La Mi'],
               'What is the key signature of the La♭ major scale?': ['Fa Do Sol', 'Fa Do Sol Re La Mi', 'Si Mi La Re Sol', 'Si Mi La Re'],
-              'The Do-Re# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Do-Re# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
               'How many 16th notes does a quarter note have?': [3, 4, 5, 8],
-              'The Mi-Fa♭ interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Mi-Fa♭ interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
               'What is the key signature of the Mi major scale?': ['Si Mi La', 'Fa Do Sol Re', 'Fa Do', 'Fa Do Sol Re La Mi Si'],
-              'The Si#-Do interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Si#-Do interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
               'How many 8th notes does a dotted quarter note have?': [3, 7, 5, 2],
               'How many 8th notes does a dotted half note have?': [3, 6, 5, 4],
-              'The Si-Do# interval is equal to a:': ['Tone', 'Semitone', 'Tri-semitone', 'Enharmonic equivalent'],
+              'The Si-Do# interval is equal to a:': ['Tone', 'Semitone', 'Trisemitone', 'Enharmonic equivalent'],
               'What is the key signature of the Sol major scale?': ['Si Mi La Re Sol Do', 'Fa Do Sol Re', 'Si Mi La Re Sol Do Fa', 'Fa'],
               }             
 
-# question and possible answers dictionary for 2nd grade
+# multiple choice quiz dictionary for 2nd grade
 second_dict = {'The Mi-La interval is a:': ['Perfect 4th', 'Perfect 5th', 'Augmented 4th', 'Diminished 5th'],
                'The Fa#-Do# interval is a:': ['Perfect 5th', 'Diminished 5th', 'Augmented 5th', 'Perfect 4th'],
                'The Mi♭-La♭ interval is a:': ['Diminished 5th', 'Perfect 4th', 'Augmented 4th', 'Diminished 4th'],
@@ -91,7 +91,7 @@ second_dict = {'The Mi-La interval is a:': ['Perfect 4th', 'Perfect 5th', 'Augme
                'The Sol-Si interval is a:': ['Diminished 3rd', 'Minor 3rd', 'Major 3rd', 'Augmented 3rd'],
                }
 
-
+# multiple choice quiz dictionary for 3rd grade
 third_dict = {'Which of the following intervals is dissonant?': ['Do-Re', 'Fa#-Do#', 'Fa-La', 'Mi-Do'],
               'Which of the following intervals is dissonant?': ['Do#-Mi', 'Si-Fa#', 'Mi-La#', 'Fa#-La#'],
               'Which of the following intervals is dissonant?': ['La-Re', 'Fa#-Re', 'Si-Mi', 'Fa-Sol'],
@@ -100,9 +100,11 @@ third_dict = {'Which of the following intervals is dissonant?': ['Do-Re', 'Fa#-D
               'What kind of chord is Re-Fa-La?': ['Major', 'Minor', 'Augmented', 'Diminished'],
               'What kind of chord is Fa-La♭-Do?': ['Major', 'Minor', 'Augmented', 'Diminished'],
               'What kind of chord is Re#-Fa#-La?': ['Major', 'Minor', 'Augmented', 'Diminished'],
-              'A minor chord belongs to:': ['3 major (I-IV-V) and 2 minor (V-VI) scales', '3 major (II-III-VI) and 2 minor (I-IV) scales', '1 major (VII) and 2 minor (II-VII) scales', '1 minor scale (III)']
+              'A diminished chord belongs to:': ['3 major (I-IV-V) and 2 minor (V-VI) scales', '3 major (II-III-VI) and 2 minor (I-IV) scales', '1 major (VII) and 2 minor (II-VII) scales', '1 minor scale (III)'],
+              'The degrees of a minor scale that have a major chord are:': ['II-III-IV', 'I-IV-V', 'V-VI', 'I-IV']
               }
 
+import os
 import pandas as pd
 import random
 import time
@@ -111,19 +113,12 @@ import time
 prep_df = pd.read_csv('preparatory.csv')
 first_df = pd.read_csv('first_grade.csv')
 second_df = pd.read_csv('second_grade.csv')
+third_df = pd.read_csv('third_grade.csv')
 
 print('-'*100)
 
 # prompt user to select a grade
-pick_grade = input('Choose your grade:\n\nPress 0 for Preparatory\nPress 1 for 1st Grade\nPress 2 for 2nd Grade\n\n: ')
-
-print('\n')
-print('-'*100)  
-time.sleep(1)
-print('Generating quiz......')
-time.sleep(2)
-print('-'*100)
-print('\n') 
+pick_grade = input('Choose your grade:\n\nPress 0 for Preparatory\nPress 1 for 1st Grade\nPress 2 for 2nd Grade\nnPress 3 for 3rd Grade\n\n: ')
 
 # generate a multiple choice question
 def generate_question(grade):
@@ -152,20 +147,50 @@ def generate_question(grade):
         # select randomly a pair of question-possible answers from the second_dict
         question, answer = random.choice(list(second_dict.items()))
         # locate the correct answer to the question in the first dataframe
-        correct_answer = first_df.Answer[first_df.Question == question].iloc[0]
+        correct_answer = second_df.Answer[second_df.Question == question].iloc[0]
         # remove the questions-answers pair from the dictionary
         second_dict.pop(question)
 
-        return question, answer, correct_answer        
+        return question, answer, correct_answer       
+
+    elif grade == '3':
+        # select randomly a pair of question-possible answers from the second_dict
+        question, answer = random.choice(list(third_dict.items()))
+        # locate the correct answer to the question in the first dataframe
+        correct_answer = third_df.Answer[third_df.Question == question].iloc[0]
+        # remove the questions-answers pair from the dictionary
+        third_dict.pop(question)
+
+        return question, answer, correct_answer
+
+    else:
+        print('You didn\'t choose a grade. Do you wish to exit? (y/n)\n') 
+        user_exit = input(': ')
+        if user_exit == 'n':
+            print('Restarting...')
+            os.system('python main.py')
+            
+        else:
+            quit()                    
 
 # create a multiple choice quiz
-def generate_quiz():        
+def generate_quiz():   
+
     # set a counter for the result
     result = 0
 
     for i in range(1):
 
         question, answer, correct_answer = generate_question(pick_grade)
+
+        # improve appearance
+        print('\n')
+        print('-'*100)  
+        time.sleep(1)
+        print('Generating quiz......')
+        time.sleep(2)
+        print('-'*100)
+        print('\n') 
 
         print(f"{i+1}. {question}\n   a. {answer[0]}\n   b. {answer[1]}\n   c. {answer[2]}\n   d. {answer[3]}\n")
 
